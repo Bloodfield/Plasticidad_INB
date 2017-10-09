90e89dab30704cf37effe0d093883afb5f3e5bea
bloodfield <edgii.ch35@gmail.com>
2017-10-09 15:28:20 -0500

    Twicks for real data

--- SAM_parce.py
+++ SAM_parce.py
@@ -59,7 +59,7 @@ with open(sys.argv[1]) as f:
         if (line[0]!="@"):
             
             # separa campos
-            Splited = re.split('\W+',line)
+            Splited = line.split()
             
             #obtiene CIGAR
             CIGAR = regex.findall(Splited[5])
@@ -67,66 +67,67 @@ with open(sys.argv[1]) as f:
             #Busqueda de los slplit reads en Gap_Form
             #   CIGAR -> Print (lineas que contengan split reads)
             
-            ##  Parte 1 encontrar los Gaps y los Clevareges
-            ##      CIGAR -> Gap_Form
-            Gap_Form=CIGAR.copy()
-            if(Gap_Form[-1][-1] in CIGAR_Tags["G"]):
-                Gap_Form[-1]=Gap_Form[-1][0:-1]+"G"
-            elif(Gap_Form[-1][-1] in CIGAR_Tags["C"]):
-                Gap_Form[-1]=Gap_Form[-1][0:-1]+"C"
-            else:
-                print ("Invalid char in ", splited[0]," : ",Gap_Form[-1][-1])
-            if debugg : print("_",Gap_Form)
-            size = len(Gap_Form)-1
-            for i in range(size):
-                if debugg : print("_ _",i," ",Gap_Form)
-                #   Si es sun gap
-                if(Gap_Form[size-1-i][-1] in CIGAR_Tags["G"]):
-                    if(Gap_Form[size-i][-1] == "G"):
-                        Gap_Form[size-1-i]=str(int(Gap_Form[size-1-i][0:-1])+int(Gap_Form[size-i][0:-1]))+"G"
-                        Gap_Form.pop(size-i)
-                    else:
-                        Gap_Form[size-1-i]=Gap_Form[size-1-i][0:-1]+"G"
-                
-                #   Si es sun Cleverage
-                elif(Gap_Form[size-1-i][-1] in CIGAR_Tags["C"]):
-                    if(Gap_Form[size-i][-1] == "C"):
-                        Gap_Form[size-1-i]=str(int(Gap_Form[size-1-i][0:-1])+int(Gap_Form[size-i][0:-1]))+"C"
-                        Gap_Form.pop(size-i)
-                    else:
-                        Gap_Form[size-1-i]=Gap_Form[size-1-i][0:-1]+"C"
-                        
-                #   Si es un caracter invalido.
+            if (len(CIGAR)>0):
+                ##  Parte 1 encontrar los Gaps y los Clevareges
+                ##      CIGAR -> Gap_Form
+                Gap_Form=CIGAR.copy()
+                if(Gap_Form[-1][-1] in CIGAR_Tags["G"]):
+                    Gap_Form[-1]=Gap_Form[-1][0:-1]+"G"
+                elif(Gap_Form[-1][-1] in CIGAR_Tags["C"]):
+                    Gap_Form[-1]=Gap_Form[-1][0:-1]+"C"
                 else:
                     print ("Invalid char in ", splited[0]," : ",Gap_Form[-1][-1])
+                if debugg : print("_",Gap_Form)
+                size = len(Gap_Form)-1
+                for i in range(size):
+                    if debugg : print("_ _",i," ",Gap_Form)
+                    #   Si es sun gap
+                    if(Gap_Form[size-1-i][-1] in CIGAR_Tags["G"]):
+                        if(Gap_Form[size-i][-1] == "G"):
+                            Gap_Form[size-1-i]=str(int(Gap_Form[size-1-i][0:-1])+int(Gap_Form[size-i][0:-1]))+"G"
+                            Gap_Form.pop(size-i)
+                        else:
+                            Gap_Form[size-1-i]=Gap_Form[size-1-i][0:-1]+"G"
                     
-            ##  Parte 2 encontrar el patron _,"(>Clev_MIN)C","(>Gap_MIN)G","(>Clev_MIN)C",_
-            
-            S=0
-            ### Maquina de estados para encontrar la secuencia
-            for i in Gap_Form:
-                if debugg: print("_ S=",S,"i[0:-1]=",i[0:-1],"i[-1]",i[-1])
-                if (S==0):
-                    if (int(i[0:-1])>=Clev_MIN and i[-1]=="C"):
-                        S=1
-                elif (S==1):
-                    if (int(i[0:-1])>=Gap_MIN and i[-1]=="G"):
-                        S=2
+                    #   Si es sun Cleverage
+                    elif(Gap_Form[size-1-i][-1] in CIGAR_Tags["C"]):
+                        if(Gap_Form[size-i][-1] == "C"):
+                            Gap_Form[size-1-i]=str(int(Gap_Form[size-1-i][0:-1])+int(Gap_Form[size-i][0:-1]))+"C"
+                            Gap_Form.pop(size-i)
+                        else:
+                            Gap_Form[size-1-i]=Gap_Form[size-1-i][0:-1]+"C"
+                            
+                    #   Si es un caracter invalido.
                     else:
-                        S=0
-                elif (S==2):
-                    if (int(i[0:-1])>=Clev_MIN and i[-1]=="C"):
+                        print ("Invalid char in ", splited[0]," : ",Gap_Form[-1][-1])
+                        
+                ##  Parte 2 encontrar el patron _,"(>Clev_MIN)C","(>Gap_MIN)G","(>Clev_MIN)C",_
+                
+                S=0
+                ### Maquina de estados para encontrar la secuencia
+                for i in Gap_Form:
+                    if debugg: print("_ S=",S,"i[0:-1]=",i[0:-1],"i[-1]",i[-1])
+                    if (S==0):
+                        if (int(i[0:-1])>=Clev_MIN and i[-1]=="C"):
+                            S=1
+                    elif (S==1):
+                        if (int(i[0:-1])>=Gap_MIN and i[-1]=="G"):
+                            S=2
+                        else:
+                            S=0
+                    elif (S==2):
+                        if (int(i[0:-1])>=Clev_MIN and i[-1]=="C"):
+                            S=3
+                        else:
+                            S=0
+                    elif (S==3):
                         S=3
                     else:
                         S=0
-                elif (S==3):
-                    S=3
-                else:
-                    S=0
-            
-            if(S==3):
-                print(Splited[0],"\t",CIGAR,"\t",Gap_Form)
-            if debugg: print(Splited[0],"\t",CIGAR,"\t",Gap_Form)
+                
+                if(S==3):
+                    print(Splited[0],"\t",CIGAR,"\t",Gap_Form)
+                if debugg: print(Splited[0],"\t",CIGAR,"\t",Gap_Form)
                     
             
 
