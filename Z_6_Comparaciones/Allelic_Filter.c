e8ee35aa95b628938e9e98346235767d656aa818
bloodfield <edgii.ch35@gmail.com>
2019-02-11 04:54:26 -0600

    Coredumped Alel fiter solved

--- Z_6_Comparaciones/Allelic_Filter.c
+++ Z_6_Comparaciones/Allelic_Filter.c
@@ -3,46 +3,66 @@
 /*	TODO
  * 	Hacer recvisión de flancos derecho e izquierdo, que en efecto sea izquierdo y derecho
  * 	Hacer guía
- * 	Funcionalizal main
+ * 	Funcionalizar main
  */
+
+//	0_Core
+
+//	*	Bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
-//	0_Core
+
+//	*	Definiciones
 #define Log_name "Log.txt"
 #define Str_len	40
-#define n_lines	120000
+#define n_lines	100000
 #define n_chr	460
 
 //	funciones
 
-//	Sistema
-int print_help(FILE *Log);
+//	*	Sistema
+int print_help();
 
-//	Algoritmo
-int read_bedcov(FILE *bedcov_fh, char Chr[][Str_len], int *flanco_A, int *flanco_B, int *cuenta, char Chr_names[][Str_len], int *Chr_ID, int *table_len, int *Chr_len, FILE *Log);
-int Filter(FILE *Overlap_fh, char Chr[][Str_len], int *flanco_A, int *flanco_B, int *cuenta, char Chr_names[][Str_len], int *Chr_ID, int table_len, int Chr_len,float th, FILE *Log);
-int get_cover(char *Chr_search,int inicio,int fin, char Chr[][Str_len], int *flanco_A, int *flanco_B, int *cuenta, char Chr_names[][Str_len], int *Chr_ID, int table_len, int Chr_len);
+//	*	Algoritmo
+int index_bedcov( );
+int read_bedcov( int *flanco_A, int *flanco_B, int *cuenta, int *table_len, char *Chr_query);
+int Filter(  float th);
+int get_cover(int inicio,int fin,  int *flanco_A, int *flanco_B, int *cuenta, int table_len);
 
-//	Arrays
+//	*	Arrays
 int search(char *Str,char Array[][Str_len],int Array_len);
 int Array_eq(char *Str1,char *Str2);
 int search_from(int query, int *Array,int Array_len, int from);
 int copy_Array(char *Str1,char *Str2);
+int Array_clear(int *Array);
+
+//	global variables
+
+//	*	Objeto de idexado del archivo bedcov_fh
+char Chr_names[n_chr][Str_len];
+int Chr_ID[n_chr];
+int Chr_len=0;
+
+//	*	Archivos
+FILE *Log;
+FILE *Overlap_fh;
+FILE *bedcov_fh;
 
 //	Z_Work
 
 int main(int argc, char *argv[]){
 	
-	//	Head
 	
-	FILE *Log;
 	Log = fopen(Log_name,"a");
 	char *bedcov;
 	char *Overlap;
+	
+	//	Input
+	
 	float th=0.0;
 		
 	if (argc != 4){
-		print_help(Log);
+		print_help();
 		fclose(Log);
 		return 1;
 	}
@@ -59,8 +79,7 @@ int main(int argc, char *argv[]){
 	
 	//	Abrir y probar archivos
 	
-	FILE *Overlap_fh;
-	FILE *bedcov_fh;
+	
 	Overlap_fh= fopen(Overlap,"r");
 	bedcov_fh = fopen(bedcov,"r");
 	
@@ -81,56 +100,39 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 	
+	//	BEGIN
 	
 	int Whatchdog=0;
 	
-	//	Leer archivo bedcov
-	int flanco_A[n_lines];
-	int flanco_B[n_lines];
-	int cuenta[n_lines];
-	char Chr[n_lines][Str_len];
-	
-	char Chr_names[n_chr][Str_len];
-	int Chr_ID[n_chr];
-	
-	int table_len=0;
-	int Chr_len=0;
-	
-	Whatchdog = read_bedcov(bedcov_fh, Chr, flanco_A, flanco_B, cuenta, Chr_names, Chr_ID, &table_len, &Chr_len, Log);
-	fclose(bedcov_fh);
-	
-	int i=0;
-// 	for (i=0;i<table_len;i++){
-// 		printf("Chr = %s\tFlanco A = %d \tFlanco B = %d \tCuenta = %d\n",Chr[i],flanco_A[i],flanco_B[i],cuenta[i]);
-// 	}
-// 	for (i=0;i<Chr_len;i++){
-// 		printf("Chr = %s\tID = %d \n",Chr_names[i],Chr_ID[i]);
-// 	}
-	
+	//	*	crea el índice de ftell de bedcov_fh
+	Whatchdog = index_bedcov( );
 	if (Whatchdog){
 		fclose(Overlap_fh);
+		fclose(bedcov_fh);
 		fclose(Log);
 		return 1;
 	}
 	
-	//	Ejecuta el filtro
-	Whatchdog =Filter(Overlap_fh, Chr, flanco_A, flanco_B, cuenta,  Chr_names, Chr_ID, table_len, Chr_len, th, Log);
+	//	*	Ejecuta el filtro
 	
+	Whatchdog =Filter( th);
 	if (Whatchdog){
 		fclose(Overlap_fh);
+		fclose(bedcov_fh);
 		fclose(Log);
 		return 1;
 	}
 	
-	//	Cierre y salida
+	//	END
 	
 	fclose(Overlap_fh);
+	fclose(bedcov_fh);
 	fclose(Log);
 	
 	return 0;
 }
 
-int print_help(FILE *Log){
+int print_help(){
 
 	fprintf(Log,"Error. Mostrando ayuda: \n");
 	fprintf(Log," \n");
@@ -154,11 +156,15 @@ int print_help(FILE *Log){
 	return 0;
 }
 
-int read_bedcov(FILE *bedcov_fh, char Chr[][Str_len], int *flanco_A, int *flanco_B, int *cuenta, char Chr_names[][Str_len], int *Chr_ID, int *table_len, int *Chr_len, FILE *Log){
-	int i=0,j=0;
+int index_bedcov(){
+	int i=0;
 	int inicio=0, fin=0, cover_score=0;
 	char Chr_previous[Str_len]={0};
+	
 	while(!feof(bedcov_fh)){
+		
+		//	temporal ftell
+		int temp=ftell(bedcov_fh);
 		
 		//	Get line
 		char Chr_read[Str_len]={0};
@@ -167,31 +173,74 @@ int read_bedcov(FILE *bedcov_fh, char Chr[][Str_len], int *flanco_A, int *flanco
 			return 1;
 		}
 		
-		copy_Array(Chr_read,Chr[i]);
-		flanco_A[i]=inicio;
-		flanco_B[i]=fin;
-		cuenta[i]=cover_score;
-		
+		//	si cambió el cromsoma, entonces guardar el elemento de índice
 		if(! Array_eq(Chr_read,Chr_previous)){
-			copy_Array(Chr_read,Chr_names[j]);
+			copy_Array(Chr_read,Chr_names[i]);
 			copy_Array(Chr_read,Chr_previous);
-			Chr_ID[j]=i;
-			j++;
-			if (j>= n_chr){
+			Chr_ID[i]=temp;
+			i++;
+			if (i>= n_chr){
 				fprintf(Log,"Archivo : Chr_names_overload\n");
 				return 1;
 			}
 		}
+	}
+	Chr_len=i;
+	return 0;
+}
+
+int read_bedcov(  int *flanco_A, int *flanco_B, int *cuenta, int *table_len, char *Chr_query){
+	
+	//	asignar lectura desde inicio de cromosoma por medio del índice
+	int idx = search(Chr_query,Chr_names,Chr_len);
+	if (idx < 0){
+		fprintf(Log,"Archivo : Not a BED_Coverage format in the bedcov file\n");
+		return 1; 
+	}
+	fseek(bedcov_fh,Chr_ID[idx],SEEK_SET);
+	
+	//	variables de estado
+	int i=0;
+	char Chr_previous[Str_len]={0};
+	copy_Array(Chr_query,Chr_previous);
+	
+	while(!feof(bedcov_fh) && Array_eq(Chr_query,Chr_previous)){
+		
+		//	Get line
+		int inicio=0, fin=0, cover_score=0;
+		char Chr_read[Str_len]={0};
+		if(fscanf(bedcov_fh,"%s\t%u\t%u\t%u\n",Chr_read,&inicio,&fin,&cover_score)!= 4){
+			fprintf(Log,"Archivo : Not a BED_Coverage format in the bedcov file\n");
+			return 1;
+		}
+		
+		//	Llenar la tabla
+		flanco_A[i]=inicio;
+		flanco_B[i]=fin;
+		cuenta[i]=cover_score;
 		i++;
-		if (j>= n_lines){
+		if (i>= n_lines){
 			fprintf(Log,"Archivo : n_lines_overload\n");
 			return 1;
 		}
 		
 	}
-	*table_len=i;
-	*Chr_len=j;
 	
+	if(i==0){
+		fprintf(Log,"Archivo : Chromosomes not complete\n");
+		return 1;
+	}
+	
+	if(!feof(bedcov_fh)){
+		flanco_A[i]=0;
+		flanco_B[i]=0;
+		cuenta[i]=0;
+		*table_len=i-1;
+		
+		return 0;
+	}
+	
+	*table_len=i;
 	return 0;
 }
 
@@ -202,73 +251,93 @@ int copy_Array(char *Str1,char *Str2){
 	}
 }
 
-int Filter(FILE *Overlap_fh, char Chr[][Str_len], int *flanco_A, int *flanco_B, int *cuenta, char Chr_names[][Str_len], int *Chr_ID, int table_len, int Chr_len,float th, FILE *Log){
+int Filter(float th){
 	
-	float landscape_coverage=0.0, del_coverage=0.0;
-	float ratio=0.0;
-	int inicio=0, fin=0;
-	int overlap_score_del=0;
-	int lenght=0;
-	int cover_score_del=0,cover_score_left=0, cover_score_right=0;
+	//	Objeto de tabla de scoring de bedvoc
+	int flanco_A[n_lines];
+	int flanco_B[n_lines];
+	int cuenta[n_lines];
+	
+	int table_len=0;
+	
+	//	variables de estado
+	char chr_act[Str_len]={0};
 	
 	while(!feof(Overlap_fh)){
 		
 		//	Get line
-		char Chr_search[Str_len]={0};
-		if(fscanf(Overlap_fh,"%s\t%u\t%u\t%u\n",Chr_search,&inicio,&fin,&overlap_score_del)!= 4){
+		int inicio=0, fin=0;
+		int overlap_score_del=0;
+		char chr_new[Str_len]={0};
+		if(fscanf(Overlap_fh,"%s\t%u\t%u\t%u\n",chr_new,&inicio,&fin,&overlap_score_del)!= 4){
 			fprintf(Log,"Archivo : Not a BED_Overlap format in the Overlap file\n");
 			return 1;
 		}
 		
-		//	search coverage scorings
+		//	llena la tabla para el chromosoma actual si es necesario
+		if(! Array_eq(chr_new, chr_act)){
+			Array_clear(flanco_A);
+			Array_clear(flanco_B);
+			Array_clear(cuenta);
+			table_len=0;
+			read_bedcov(flanco_A, flanco_B, cuenta, &table_len,chr_new);
+			copy_Array(chr_new, chr_act);
+		}
 		
-		cover_score_del = get_cover(Chr_search,inicio,fin,Chr,flanco_A,flanco_B,cuenta,Chr_names,Chr_ID,table_len,Chr_len);
-		cover_score_left = get_cover(Chr_search,inicio-20,inicio,Chr,flanco_A,flanco_B,cuenta,Chr_names,Chr_ID,table_len,Chr_len);
-		cover_score_right = get_cover(Chr_search,fin,fin+20,Chr,flanco_A,flanco_B,cuenta,Chr_names,Chr_ID,table_len,Chr_len);
+		//	search coverage scorings, se tiene garantizado que solo hay el cromosoma actual está en el buffer
+		int cover_score_del = get_cover(inicio,fin,flanco_A,flanco_B,cuenta,table_len);
+		int cover_score_left = get_cover(inicio-20,inicio,flanco_A,flanco_B,cuenta,table_len);
+		int cover_score_right = get_cover(fin,fin+20,flanco_A,flanco_B,cuenta,table_len);
 		
-		if (cover_score_del==-1 || cover_score_left==-1 || cover_score_right==-1){
+		int Whatchdog=(cover_score_del==-1 || cover_score_left==-1 || cover_score_right==-1);
+		if (Whatchdog){
 			fprintf(Log,"Cover file not complete\n");
 			return 1;
 		}
 		
 		//	compute fraction
-		lenght = fin -inicio;
+		int lenght = fin -inicio;
 		// 		printf("lenght = %u\n",lenght);
-		landscape_coverage=(cover_score_left+cover_score_right)/40.0;
+		float landscape_coverage=(cover_score_left+cover_score_right)/40.0;
 		// 		printf("land cov = %0.6f\n",landscape_coverage);
-		del_coverage=cover_score_del/(1.0*lenght);
+		float del_coverage=cover_score_del/(1.0*lenght);
 		// 		printf("del cov = %0.6f\n",del_coverage);
-		ratio=del_coverage/landscape_coverage;
+		float ratio=del_coverage/landscape_coverage;
 		// 		printf("ratio = %0.6f\n",ratio);
 		
 		//	check and print
-		if((ratio < 1.0-th && ratio >0.5+th) || (ratio <0.5-th && ratio > th)){
-			printf("%s\t%u\t%u\t%.5f\t%u\n",Chr_search,inicio,fin,ratio,overlap_score_del);
+		Whatchdog = (ratio < 1.0-th && ratio >0.5+th) || (ratio <0.5-th && ratio > th);
+		if(Whatchdog){
+			printf("%s\t%u\t%u\t%.5f\t%u\n",chr_new,inicio,fin,ratio,overlap_score_del);
 		}
 	}
 	
 	return 0;
 }
 
-int get_cover(char *Chr_search,int inicio,int fin, char Chr[][Str_len], int *flanco_A, int *flanco_B, int *cuenta, char Chr_names[][Str_len], int *Chr_ID, int table_len, int Chr_len){
+int get_cover(int inicio,int fin, int *flanco_A, int *flanco_B, int *cuenta, int table_len){
 	int idx=0;
-	int Chr_end=0;
-	idx = search(Chr_search,Chr_names,Chr_len);
-	if(idx <0){return -1;}
-	if (idx < Chr_len-1){
-		Chr_end=Chr_ID[idx+1];
-	}else{
-		Chr_end=table_len;
-	}
-	idx = Chr_ID[idx];
-	idx = search_from(inicio,flanco_A,Chr_end,idx);
-	if(idx <0){return -1;}
-	Chr_end=idx;
-	while(flanco_A[Chr_end]==flanco_A[idx] && Chr_end < table_len -1){Chr_end++;}
-	if(flanco_A[Chr_end]==flanco_A[idx] && Chr_end==table_len -1){Chr_end++;}
-	idx = search_from(fin,flanco_B,Chr_end,idx);
-	if(idx <0){return -1;}
 	
+	//	busca la primera coordenada
+	idx = search_from(inicio,flanco_A,table_len,0);
+	if(idx <0){
+		fprintf(Log,"get_cover (1) : bedcov not completed, %d not found \n",inicio);
+		return -1;
+	}
+	
+	//	busca límite de busqueda para 2da coordenada
+	int coord_end=idx;
+	while(flanco_A[coord_end]==flanco_A[idx] && coord_end < table_len -1){coord_end++;}
+	if(flanco_A[coord_end]==flanco_A[idx] && coord_end==table_len -1){coord_end++;}
+	
+	//	busca la segunda coordenada
+	idx = search_from(fin,flanco_B,coord_end,idx);
+	if(idx <0){
+		fprintf(Log,"get_cover (2) : bedcov not completed, %d not found \n",fin);
+		return -1;
+	}
+	
+	//	regresa el valor de la cuenta
 	return cuenta[idx];
 }
 
@@ -301,4 +370,11 @@ int search_from(int query, int *Array,int Array_len, int from){
 		}
 	}
 	return -1;
+}
+
+int Array_clear(int *Array){
+	int i=0;
+	for(i=0; i<n_lines;i++){
+		Array[i]=0;
+	}
 }
