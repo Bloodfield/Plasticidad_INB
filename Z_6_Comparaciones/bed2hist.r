5e07b81277dc51be0afd876e3ef7bf3426c50cc1
bloodfield <edgii.ch35@gmail.com>
2019-03-22 14:13:03 -0600

    fix : bed2hist.r : graficos mejor presentados

--- Z_6_Comparaciones/bed2hist.r
+++ Z_6_Comparaciones/bed2hist.r
@@ -16,14 +16,14 @@ img_height=350;
 bed_table = read.table("stdin");
 
 centromere_coords=paste(path.expand("~"),"/bin/centromere_coords.bed",sep="")
-centromere_table = read.table(centromere_coords);
-# centromere_table = read.table("/home/bloodfield/W_Root/1_Scy/Biología_Computer_Bioinformática/Plasticidad/Z_6_Comparaciones/centromere_coords.bed");
+# centromere_table = read.table(centromere_coords);
+centromere_table = read.table("/home/bloodfield/W_Root/1_Scy/Biología_Computer_Bioinformática/Plasticidad/Z_6_Comparaciones/centromere_coords.bed");
 row.names(centromere_table) <- centromere_table[,1];
 centromere_table <- centromere_table[,2-3]
 
 Chr_end = paste(path.expand("~"),"/bin/human.hg38.genome",sep="")
-Chr_end_table = read.table(Chr_end);
-# Chr_end_table = read.table("/home/bloodfield/W_Root/1_Scy/Biología_Computer_Bioinformática/Plasticidad/Z_6_Comparaciones/human.hg38.genome");
+# Chr_end_table = read.table(Chr_end);
+Chr_end_table = read.table("/home/bloodfield/W_Root/1_Scy/Biología_Computer_Bioinformática/Plasticidad/Z_6_Comparaciones/human.hg38.genome");
 row.names(Chr_end_table) <- Chr_end_table[,1]
 
 #	For every Chromosome
@@ -116,15 +116,20 @@ for (Chromosome in chr_list){
 		ylab="Frecuencia de deleciones recurrentes",
 		xlim=c(0, Chr_end_table[Chromosome,2]), 
 		ylim=c(0, height),
+		lwd = 0.5,
 		axes=TRUE);
+	Step_text = paste("Step = ",round(step)," bases",sep="");
+	mtext(Step_text, side=3)
 	lines(	chr_table_x,
 		chr_table_y_avr,
 		type="h",
-		col="mediumseagreen");
+		col="mediumseagreen",
+		lwd = 0.5);
 	lines(	chr_table_x,
 		chr_table_y_min,
 		type="h",
-		col="blue");
+		col="blue",
+		lwd = 0.5);
 # 	axis(side=1,pos=Chr_end_table[Chromosome,2])
 # 	axis(side=2,pos=height)
 	
